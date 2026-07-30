a57d3eadefe97fabe66a212ed1a10850e18f582b
Oleg Polyakov <olegpolyakov@outlook.com>
2026-07-31 00:32:12 +0300

    Correctly configure ws

--- src/ws.ts
+++ src/ws.ts
@@ -5,19 +5,26 @@ import type { Server } from '@olegpolyakov/backend/server';
 import type Context from '@/context.ts';
 
 export default ({ models: { Task, Project } }: Context) => (server: Server) => {
-    const wss = new WebSocketServer({ server: server.server });
+    const wss = new WebSocketServer({
+        server: server.server,
+        path: '/ws'
+    });
 
-    console.log('WebSocket server is listening');
+    wss.on('listening', () => {
+        console.log(`WebSocket server is running on ${server.host}:${server.port}${wss.options.path}`);
+    });
 
+    const models = [Task, Project];
     const pipeline = [{
         $match: { operationType: { $in: ['insert', 'update', 'delete'] } }
     }];
+    const options = {
+        fullDocument: 'updateLookup', // ensures the full document is returned on updates
+        hydrate: true
+    };
 
-    [Task, Project].forEach(model => {
-        const changeStream = model.watch(pipeline, {
-            fullDocument: 'updateLookup', // ensures the full document is returned on updates
-            hydrate: true
-        }); 
+    models.forEach(model => {
+        const changeStream = model.watch(pipeline, options); 
 
         changeStream.on('change', event => {
             const payload = JSON.stringify({
@@ -35,7 +42,7 @@ export default ({ models: { Task, Project } }: Context) => (server: Server) => {
         });
 
         changeStream.on('error', error => {
-            console.error('Mongoose Change Stream Error:', error);
+            console.error('Change Stream Error:', error);
         });
     });  
 };
